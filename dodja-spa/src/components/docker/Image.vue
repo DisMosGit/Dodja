1d2bf7ac6cd52dc5f1c962d2381bccc5ae4d40c7
Denis <dissurgu@yandex.ru>
2021-06-16 00:22:29 +0500

    add spa Image view

--- dodja-spa/src/components/docker/Image.vue
+++ dodja-spa/src/components/docker/Image.vue
@@ -1,36 +1,23 @@
 <template>
-  <b-jumbotron border-variant="dark" class="col-md-auto m-1 p-1">
+  <b-jumbotron border-variant="dark" class="col-md-auto m-1 p-1 shadow-sm">
     <h2>
-      <b-badge :variant="this.colors[container.State]">{{
-        container.Id | cut(0, 10)
-      }}</b-badge>
-      {{ container.Names | dockerNames }}
-      <b-badge variant="light">{{ container.Created | toDate("ms") }}</b-badge>
+      <b-badge>{{ image.Id | cut(7, 16) }}</b-badge>
+      <b-badge variant="light">{{ image.Created | toDate("ms") }}</b-badge>
     </h2>
-    <p>{{ container.State }}: {{ container.Status }}</p>
-    <p>{{ container.Image }}: {{ container.ImageID | cut(0, 20) }}</p>
     <hr class="my-2" />
-    <p>
-      {{ container.Ports | dockerPorts }}
+    <p class="text-break m-0" v-for="tag in image.RepoTags" v-bind:key="tag">
+      {{ tag }}
     </p>
-    <p>
-      {{ container.Mounts | dockerMounts }}
-    </p>
+    <hr />
     <b-button-group>
       <b-button variant="primary" v-on:click="goDetail()">
         Detail
       </b-button>
-      <b-button v-if="is_on" variant="outline-danger" v-on:click="goRefresh()">
-        goRefresh
+      <b-button variant="danger" v-on:click="goStop()">
+        Pull
       </b-button>
-      <b-button v-if="is_on" variant="warning" v-on:click="goRestart()">
-        Restart
-      </b-button>
-      <b-button v-if="is_on" variant="danger" v-on:click="goStop()">
-        Stop
-      </b-button>
-      <b-button v-if="!is_on" variant="success" v-on:click="goStart()">
-        Start
+      <b-button variant="info" v-on:click="goStart()">
+        Tag
       </b-button>
     </b-button-group>
   </b-jumbotron>
@@ -42,61 +29,34 @@ import { mapActions } from "vuex";
 export default {
   name: "Image",
   props: ["image", "host_id"],
-
-  data() {
-    return {
-      colors: {
-        paused: "warning",
-        restarting: "info",
-        running: "success",
-        dead: "danger",
-        created: "primary",
-        exited: "secondary"
-      }
-    };
-  },
-  computed: {
-    is_on: function() {
-      switch (this.container.State) {
-        case "paused":
-        case "dead":
-        case "created":
-        case "exited":
-          return false;
-        case "running":
-        case "restarting":
-          return true;
-        default:
-          return false;
-      }
-    }
-  },
   methods: {
     ...mapActions(["executeHost"]),
-    goDetail: function() {
-      this.$router.push({
-        name: "HostContainerDetail",
-        params: {
-          id: this.host_id,
-          obj: this.container.Id,
-          pre_container: this.container
-        },
-        props: true
-      });
+    goTag: function() {
+      // TODO:
+      // this.$router.push({
+      //   name: "HostContainerDetail",
+      //   params: {
+      //     id: this.host_id,
+      //     obj: this.container.Id,
+      //     pre_container: this.container
+      //   },
+      //   props: true
+      // });
     },
-    goRemove: function() {
-      this.executeHost({
-        id: this.host_id,
-        data: {
-          command: "api.remove_image",
-          args: { container: this.image.Id }
-        }
-      }).then(() => {
-        this.$router.push({
-          name: "HostDocker",
-          params: { id: this.host.id }
-        });
-      });
+    goPush: function() {
+      // TODO:
+      // this.executeHost({
+      //   id: this.host_id,
+      //   data: {
+      //     command: "api.remove_image",
+      //     args: { container: this.image.Id }
+      //   }
+      // }).then(() => {
+      //   this.$router.push({
+      //     name: "HostDocker",
+      //     params: { id: this.host.id }
+      //   });
+      // });
     }
   }
 };
--- dodja-spa/src/views/docker/HostImageList.vue
+++ dodja-spa/src/views/docker/HostImageList.vue
@@ -1,9 +1,165 @@
 <template>
-  <div></div>
+  <div>
+    <h1>HostImageList</h1>
+    <b-button variant="success" v-on:click="goRefreshImageList()">
+      Refresh list
+    </b-button>
+    <div class="row justify-content-md-center mx-0">
+      <DockerImage
+        v-for="image in image_list"
+        v-bind:key="image.Id"
+        v-bind:image="image"
+        v-bind:host_id="host_id"
+      ></DockerImage>
+    </div>
+    <div class="container m-2 p-1 shadow">
+      <label>Type a dockerfile text</label>
+      <CodeTextarea
+        ref="build-text"
+        default="FROM image:version\nENTRYPOINT"
+        mode="dockerfile"
+      ></CodeTextarea>
+      <hr />
+      <label>Type build parameters</label>
+      <CodeTextarea
+        ref="build-args"
+        default='tag: "example:version"\nnocache: false'
+        mode="yaml"
+      ></CodeTextarea>
+      <hr />
+      <label>Build result</label>
+      <CodeTextarea
+        ref="build-result"
+        default=""
+        :is_read="true"
+        mode="javascript"
+      ></CodeTextarea>
+      <b-button-group>
+        <b-button @click="tryBuild" variant="primary">
+          {{ $tc("build") | capitalize }} {{ $tc("image") | capitalize }}
+        </b-button>
+        <b-button @click="tryReset" variant="warning">
+          {{ $tc("reset") | capitalize }}
+        </b-button>
+      </b-button-group>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import Image from "@/components/docker/Image";
+import CodeTextarea from "@/components/CodeTextarea";
+
 export default {
-  name: "HostImageList"
+  name: "HostImageList",
+  props: ["host_id"],
+  components: {
+    CodeTextarea,
+    DockerImage: Image
+  },
+  data() {
+    return {
+      image_list: null,
+      build_tags: ["example:version"],
+      image: {
+        Containers: -1,
+        Created: 1623666697,
+        Id:
+          "sha256:315ba3f3a4402bd8cc89f66871eda1f606831ef4ce40628b8d2030cc94763914",
+        Labels: null,
+        ParentId:
+          "sha256:8a99bea7b90ca4214454d092da1291ab16ce5bb0693d33eb544c624732955e82",
+        RepoDigests: null,
+        RepoTags: ["e60b9c3b1cf34cd758dfecfe9314d906_celery:latest"],
+        SharedSize: -1,
+        Size: 274706828,
+        VirtualSize: 274706828
+      }
+    };
+  },
+  computed: {
+    build_text: {
+      get: function() {
+        return this.$refs["build-text"].text;
+      },
+      set: function(value) {
+        this.$refs["build-text"].editText(value);
+      }
+    },
+    build_args: {
+      get: function() {
+        return this.$refs["build-args"].text;
+      },
+      set: function(value) {
+        this.$refs["build-args"].editText(value);
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["executeHost"]),
+    goRefreshImageList: function() {
+      this.executeHost({
+        id: this.host_id,
+        data: {
+          command: "api.images",
+          args: {
+            all: false
+          }
+        }
+      })
+        .then(response => {
+          this.image_list = response.data.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    goRefreshImageListWithFilters: function(filters) {
+      this.executeHost({
+        id: this.host_id,
+        data: {
+          command: "api.images",
+          args: {
+            all: true,
+            filters: filters
+          }
+        }
+      })
+        .then(response => {
+          this.image_list = this.image_list.map(
+            obj => response.data.data.find(o => o.Id === obj.Id) || obj
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    tryBuild: function() {
+      this.executeHost({
+        id: this.host_id,
+        data: {
+          command: "api.build",
+          args: Object.assign({}, this.build_args, {
+            fileobj: this.build_text
+          })
+        }
+      })
+        .then(response => {
+          this.$refs["build-result"].addText(response.data);
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    tryReset: function() {
+      this.build_text = "FROM image:version\nENTRYPOINT";
+      this.build_args = 'tag: "example:version"\nnocache: false';
+    }
+  },
+  mounted() {
+    this.goRefreshImageList();
+  }
 };
 </script>
